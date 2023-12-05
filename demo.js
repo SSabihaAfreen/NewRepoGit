fe1956a2eaabe81c23123fe63b8ba6372f28ae20
SSabihaAfreen <afrahmanhaadnan>
2023-12-05 12:12:34 +0530

    another commit

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
-var header=document.querySelector('#main-header');
+/*var header=document.querySelector('#main-header');
 header.style.borderBottom='solid 4px #ccc';
 var input=document.querySelector('input');
 input.value='Hello world';
@@ -7,4 +7,43 @@ submit.value="SEND";
 var item=document.querySelector('.list-group-item');
 item.style.color='red';
 var lastItem=document.querySelector('.list-group-item-child');
-lastItem.style.color='blue';
+lastItem.style.color='blue';*/
+var itemList=document.querySelector('#items');
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor='#f4f4f4';
+console.log(itemList.parentNode.parentNode.parentNode);
+
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor='#f4f4f4';
+console.log(itemList.parentElement.parentElement.parentElement);
+
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor='yellow';
+//firstChild
+console.log(itemList.firstChild);
+//firstElementChild
+//console.log(itemList.firstElementChild());
+//itemList.firstElementChild.textContent='Hello';
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent='Hello 4';
+//nextsiblings
+console.log(itemList.nextSibling);
+//nextElementSibling
+console.log(itemList.nextElementSibling);
+//previousSibling
+console.log(itemList.previousSibling);
+//previousElementSibling
+console.log(itemList.previousElementSibling);
+////createElement
+var newDiv=document.createElement('div');
+newDiv.className='hello';
+newDiv.id='hello1';
+newDiv.setAttribute('title','Hello Div');
+var newDivText=document.createTextNode('Hello World');
+newDiv.appendChild(newDivText);
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+console.log(newDiv);
+newDiv.style.fontsize='30px';
+container.insertBefore(newDiv,h1);
